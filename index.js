48e601def385bf72183e513ef4b2f393c4c1237d
Sean Harrigan <SeanHarrigan8@gmail.com>
2024-01-16 07:03:16 -0600

    tests written

--- index.js
+++ index.js
@@ -1,12 +1,58 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const { Shape, Circle, Square, Triangle } = require('./lib/shapes');
 
 inquirer 
     .prompt([
+        {
         type: 'input',
-        name: 'name',
-        message: 'What is your name?'
-    ])
-    .then(answers => { 
-        console.log(`Hi ${answers['name']}!`)
-    });         
+        name: 'text',
+        message: 'Enter up to three chracters for your logo',
+            validate: function(value) {
+                if (value.length <= 3) {
+                    return true;
+                }
+                return 'Please enter UP to 3 characters.';
+            }
+            },
+
+            {
+                type: 'input',
+                name: 'textColor',
+                message: 'Please enter a color or hexadecimal value for the text'
+            },
+            {
+                type: 'list',
+                name: 'shape',
+                message: 'Choose a shape:',
+                choices: ['circle', 'triangle', 'square']
+            },
+            {
+                type: 'input',
+                name: 'shapeColor',
+                message: 'Enter a color keyword or hexadecimal number for the shape color:'
+            }
+        ])
+        .then(answers => { 
+            let shape;
+            switch (answers.shape) {
+                case 'triangle':
+                    shape = new Triangle();
+                    break;
+                case 'circle':
+                    shape = new Circle();
+                    break;
+                case 'square':
+                    shape = new Square();
+                    break;
+            }           
+            shape.setColor(answers.shapeColor);
+
+            const svg = shape.render();
+
+            fs.writeFile('logo.svg', svg, (err) => {
+                if (err) throw err;
+                console.log('generated logo.svg and saved! congratulations!');
+            });
+
+        });         
